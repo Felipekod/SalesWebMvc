b5ad5f39e456d59ef5a504a677ca9d21bcf1698f
Felipekod <55560328+Felipekod@users.noreply.github.com>
2021-10-13 13:20:24 -0400

    Initial commit

---
+++ Controllers/DepartementsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartementsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Departement> liste = new List<Departement>();
+            liste.Add(new Departement { Id= 1, Nom = "Eletroniques" });
+            liste.Add(new Departement { Id = 2, Nom = "Mode" });
+
+
+            return View(liste);
+        }
+    }
+}
---
+++ Models/Departement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SalesWebMvc.Models
+{
+    public class Departement
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+
+        public Departement()
+        {
+        }
+    }
+}
